41866cb93ed376394b169ab8ceaba6d17261694e
ajurisic <andelo.jurisic@gmail.com>
2024-06-05 16:07:57 +0200

    Identity schema added. Fixed documentation issue. Added migrations

---
+++ src/MTCA.API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTCA.API.Abstractions;
+using MTCA.Application.Commons.Models;
+using MTCA.Domain.Authorization;
+using MTCA.Infrastructure.Authentication;
+
+namespace MTCA.API.Controllers;
+/// <summary>
+/// UserController
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ApiController
+{
+
+    /// <summary>
+    /// UserController constructor
+    /// </summary>
+    /// <param name="sender"></param>
+    public UserController(ISender sender)
+        : base(sender)
+    {
+    }
+
+
+
+    //[HasPermission(ActionCatalog.Search, ResourceCatalog.Users)]
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok();
+
+    }
+}
